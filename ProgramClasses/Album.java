e2e66a5e996428bc0c52f4766015d7b4a300af2a
chewmonguy225 <mikeylupa225@gmail.com>
2024-10-23 17:47:32 -0500

    Initial Upload

---
+++ ProgramClasses/Album.java
@@ -0,0 +1,7 @@
+public class Album extends ReviewableItem {
+    private Song[] trackList;
+    public Album(String id, String title, Artist artist, Song[] trackList){
+        super(id, title, artist);
+        this.trackList = trackList;
+    }
+}
---
+++ ProgramClasses/Song.java
@@ -0,0 +1,9 @@
+public class Song extends ReviewableItem{
+    private Album album;
+    private int trackNumber; //where the song appears in the album
+
+    public Song(String id, String title, Artist artist, Album album, int trackNumber){
+        super(id, title, artist);
+        this.trackNumber = trackNumber;
+    }
+}
