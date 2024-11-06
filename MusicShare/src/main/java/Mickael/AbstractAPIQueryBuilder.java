dbefbf09c57e05fe26b02e318f11f28e11fae0ad
magust4 <149904845+magust4@users.noreply.github.com>
2024-11-06 17:54:28 -0600

    Delete MusicShare/src/main/java/Mickael directory

--- MusicShare/src/main/java/Mickael/AbstractAPIQueryBuilder.java
+++
@@ -1,9 +0,0 @@
-package Mickael;
-import java.util.ArrayList;
-
-//TRACK TO SONG REPLACE
-public abstract class AbstractAPIQueryBuilder {
-    public abstract ArrayList<Album> searchAlbum(String albumTitle);
-    // public abstract ArrayList<Song> searchSong(String songTitle);
-    // public abstract ArrayList<Artist> searchArtists(String artistName);
-}
