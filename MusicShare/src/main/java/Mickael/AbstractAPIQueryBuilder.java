9468070249a1eaa3a994476a77c7db3b2cfeac1e
magust4 <149904845+magust4@users.noreply.github.com>
2024-11-13 12:35:00 -0600

    Delete MusicShare directory

--- MusicShare/src/main/java/Mickael/AbstractAPIQueryBuilder.java
+++
@@ -1,9 +0,0 @@
-package Mickael;
-
-
-//TRACK TO SONG REPLACE
-public abstract class AbstractAPIQueryBuilder {
-    public abstract Album searchAlbum(String albumTitle);
-    //public abstract Song searchSong(String songTitle);
-    //public abstract Artist searchArtists(String artistName);
-}
