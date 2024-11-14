6b7c5191d9e724b4f1e727d80d2cd8132cbf4ac9
Dan Rascop <80607356+drascop@users.noreply.github.com>
2024-11-14 15:07:36 -0600

    update dbhandler and query executor

--- Program/DBHandler.java
+++ Program/DBHandler.java
@@ -42,4 +42,45 @@ public class DBHandler {
             return false;
         }
     }
+
+
+    /**
+    * Adds a new friend relationship into the database after confirming that the user exists.
+    *
+    * @param username The username of the user that is logged in.
+    * @param friend_username The username of the friend to be added.
+    * @return True if the friend is added succesfully, false otherwise.
+    */
+    public boolean addFriend(String username, String friendUsername)
+    {
+        try {
+            if(queryExecutor.checkUserExists(friendUsername)){
+                return false;
+            }
+            else {
+                queryExecutor.addFriend(username, friendUsername);
+                return true;
+            }
+        } 
+        catch (Exception e) {
+            return false;
+        }
+    }
+
+
+    public boolean addSongToDB(String name, String artist, String album)
+    {
+        try {
+            if(queryExecutor.checkSongInDB(name, artist, album)){
+                return true; 
+            }
+            else {
+                queryExecutor.addSongToDB(name, artist, album);
+                return true;
+            }
+        } 
+        catch (Exception e) {
+            return false;
+        }
+    }
 }
