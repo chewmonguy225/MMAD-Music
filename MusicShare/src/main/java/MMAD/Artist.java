a1cfad4be7544c1efa681f4dac771e268d3d668b
magust4 <149904845+magust4@users.noreply.github.com>
2024-11-19 15:01:33 -0600

    Add files via upload

---
+++ MusicShare/src/main/java/MMAD/Artist.java
@@ -0,0 +1,21 @@
+package MMAD;
+
+import java.util.*;
+public class Artist extends Item {
+    private String id;
+    private String name;
+
+    public Artist(String id, String name){
+        super(id, name);
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getID(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
---
+++ MusicShare/src/main/java/MMAD/SpotifyClientCredentials.java
@@ -0,0 +1,28 @@
+package MMAD;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class SpotifyClientCredentials {
+    private String filePath = "src/main/java/MMAD/clientCredentials.txt";
+    private static String clientID;
+    private static String clientSecret;
+
+    public SpotifyClientCredentials() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            clientID = reader.readLine(); 
+            clientSecret = reader.readLine(); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String getClientID(){
+        return clientID;
+    }
+
+    public static String getClientSecret(){
+        return clientSecret;
+    }
+
+}
