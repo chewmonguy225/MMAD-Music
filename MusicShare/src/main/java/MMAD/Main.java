a685ff0225f0fc624df8c1f79dde0323d9883f13
chewmonguy225 <mikeylupa225@gmail.com>
2024-11-28 12:53:27 -0600

    Made updates to controller class

--- MusicShare/src/main/java/MMAD/Main.java
+++ MusicShare/src/main/java/MMAD/Main.java
@@ -1,7 +1,7 @@
 package MMAD;
 public class Main {
     //testing user creation
-    Login cred = new Login();
+    Login cred = new Login("username", "password");
     User user1 = new User(cred);
 
     
--- MusicShare/src/main/java/MMAD/ReviewHandler.java
+++ MusicShare/src/main/java/MMAD/ReviewHandler.java
@@ -14,13 +14,15 @@ public class ReviewHandler {
         return rh;
     }
 
-    public void createReview(Login login, Item item){
+    public void createReview(Login login, Item item, String description, int rating){
+        Review r = new Review(item, description, rating);//creates review object
+
         //Use the following condition to find the item in the DB
         if(item instanceof Song){
             //search for song with item.getID()
-            //if song exists, record the review input and save it to DB
+            //if song exists, save review object "r" in the DB
         }else if(item instanceof Album){
-
+            //search if album then will record in the DB as an album object
         }else if (item instanceof Artist){
 
         } else {
