cce5f7bd482fca75a79565b43eda31a1748ded93
chewmonguy225 <mikeylupa225@gmail.com>
2024-11-27 18:51:19 -0600

    added comment

--- MusicShare/src/main/java/MMAD/ReviewHandler.java
+++ MusicShare/src/main/java/MMAD/ReviewHandler.java
@@ -15,8 +15,10 @@ public class ReviewHandler {
     }
 
     public void createReview(Login login, Item item){
+        //Use the following condition to find the item in the DB
         if(item instanceof Song){
-            
+            //search for song with item.getID()
+            //if song exists, record the review input and save it to DB
         }else if(item instanceof Album){
 
         }else if (item instanceof Artist){
