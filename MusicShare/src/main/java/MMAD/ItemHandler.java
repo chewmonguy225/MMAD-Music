1501ce7bc8eec9c359047c61d0d648a0246e1710
magust4 <149904845+magust4@users.noreply.github.com>
2024-12-04 10:42:43 -0600

    Update ItemHandler.java

--- MusicShare/src/main/java/MMAD/ItemHandler.java
+++ MusicShare/src/main/java/MMAD/ItemHandler.java
@@ -8,6 +8,8 @@ public class ItemHandler {
     private static SpotifyAPIQueryBuilder api = SpotifyAPIQueryBuilder.access();
     DBHandler dbh = new DBHandler();
     private final int itemsPerPage = 5;
+    private Song selectedSong;
+
 
     private ItemHandler() {
 
@@ -22,70 +24,129 @@ public class ItemHandler {
 
     /**
      * adds song, album and artist to the DB for the song object
+     * 
      * @param song
      */
-    public void addSongToDB(Song song){
+    public void addSongToDB(Song song) {
         addAlbumToDB(song.getAlbum());
         dbh.addSongToDB(song);
     }
 
     /**
      * adds the album and the artist to the DB for the album object
+     * 
      * @param album
      */
-    public void addAlbumToDB(Album album){
+    public void addAlbumToDB(Album album) {
         addArtistToDB(album.getArtist());
         dbh.addAlbumToDB(album);
     }
 
     /**
      * adds the artist to the DB
+     * 
      * @param artist
      */
-    public void addArtistToDB(Artist artist){
+    public void addArtistToDB(Artist artist) {
         dbh.addArtistToDB(artist);
     }
 
     public Song createSongFromID(int id) {
-
-        ArrayList<String> songInfo = dbh.getSong(id);
-        Artist artist = new Artist(Integer.parseInt(songInfo.get(5)), songInfo.get(6), songInfo.get(3));
-        Album album = new Album(Integer.parseInt(songInfo.get(7)), songInfo.get(8), songInfo.get(4), artist);
-        Song song = new Song(id, songInfo.get(1), songInfo.get(2), artist, album);
-        return song;
         // use song id to get all other song information.
         // calls the dbhandler to retrieve each piece of song info in order to create
         // and return a song object
-        
-        //Song theSong = new Song(null, null, null, null);
-        //return theSong;
+        Song theSong = new Song(null, null, null, null);
+        return theSong;
     }
 
     public int searchSong(String songTitle, UI ui, Display d) {
         ArrayList<Song> results = api.searchSong(songTitle);
+
         int totalPages = (int) Math.ceil((double) results.size() / itemsPerPage);
         int currentPage = 1;
-        d.displaySongs(results, currentPage, totalPages);
-        int option = ui.getInt();
 
-        Song selectedSong = null;
+        d.displaySongSearchResult(results, currentPage, totalPages);
+        int option = ui.getInt();
+ 
+        selectedSong = null;
         while (selectedSong == null && option != -1) {
             if (option >= 1 && option <= 5) {
                 selectedSong = results.get((currentPage * itemsPerPage) + (option - 1));
-            }else if (option == 6 && currentPage != totalPages) {
+            } else if (option == 6 && currentPage != totalPages) {
                 currentPage++;
-                d.displaySongs(results, currentPage, totalPages);
+                d.displaySongSearchResult(results, currentPage, totalPages);
                 option = ui.getInt();
-        } else if (option == 7 && currentPage != 1) {
+            } else if (option == 7 && currentPage != 1) {
                 currentPage--;
-                d.displaySongs(results, currentPage, totalPages);
+                d.displaySongSearchResult(results, currentPage, totalPages);
                 option = ui.getInt();
             } else if (option == 7 && currentPage == 1) {
-                option = -1; 
+                option = -1;
             }
         }
         addSongToDB(selectedSong);
         return option;
     }
 
+    public int searchAlbum(String albumTitle, UI ui, Display d) {
+        ArrayList<Album> results = api.searchAlbum(albumTitle);
+
+        int totalPages = (int) Math.ceil((double) results.size() / itemsPerPage);
+        int currentPage = 1;
+
+        d.displayAlbumSearchResult(results, currentPage, totalPages);
+        int option = ui.getInt();
+ 
+        Album selected = null;
+        while (selected == null && option != -1) {
+            if (option >= 1 && option <= 5) {
+                selected = results.get((currentPage * itemsPerPage) + (option - 1));
+            } else if (option == 6 && currentPage != totalPages) {
+                currentPage++;
+                d.displayAlbumSearchResult(results, currentPage, totalPages);
+                option = ui.getInt();
+            } else if (option == 7 && currentPage != 1) {
+                currentPage--;
+                d.displayAlbumSearchResult(results, currentPage, totalPages);
+                option = ui.getInt();
+            } else if (option == 7 && currentPage == 1) {
+                option = -1;
+            }
+        }
+        addAlbumToDB(selected);
+        return option;
+    }
+
+    public int searchArtist(String artist, UI ui, Display d) {
+        ArrayList<Artist> results = api.searchArtist(artist);
+
+        int totalPages = (int) Math.ceil((double) results.size() / itemsPerPage);
+        int currentPage = 1;
+
+        d.displayArtistSearchResult(results, currentPage, totalPages);
+        int option = ui.getInt();
+ 
+        Artist selected = null;
+        while (selected == null && option != -1) {
+            if (option >= 1 && option <= 5) {
+                selected = results.get((currentPage * itemsPerPage) + (option - 1));
+            } else if (option == 6 && currentPage != totalPages) {
+                currentPage++;
+                d.displayArtistSearchResult(results, currentPage, totalPages);
+                option = ui.getInt();
+            } else if (option == 7 && currentPage != 1) {
+                currentPage--;
+                d.displayArtistSearchResult(results, currentPage, totalPages);
+                option = ui.getInt();
+            } else if (option == 7 && currentPage == 1) {
+                option = -1;
+            }
+        }
+        addArtistToDB(selected);
+        return option;
+    }
+
+    public Song getSelectedSong(){
+        return selectedSong;
+    }
 }
