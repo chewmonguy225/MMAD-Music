3c36060a02f76196c68d85e8117a73b50828d065
magust4 <149904845+magust4@users.noreply.github.com>
2024-12-08 21:16:57 -0600

    9:16

--- MusicShare/src/test/java/com/example/SearchTest.java
+++ MusicShare/src/test/java/com/example/SearchTest.java
@@ -15,13 +15,24 @@ import java.util.ArrayList;
 public class SearchTest {
 
     private SpotifyAPIQueryBuilder api;
-    
+
     @Before
     public void setUp() {
         api = SpotifyAPIQueryBuilder.access();
     }
 
-    @Test
+    @Test // Test search with an empty string
+    public void testingEmptySongSearch() {
+        String songTitle = "";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            api.searchSong(songTitle);
+        });
+
+        assertEquals("Song title cannot be null or empty", exception.getMessage());
+    }
+
+    @Test // search with empty String
     public void testingSongSearch() {
         String songTitle = "let it be";
         ArrayList<Song> theSongs = api.searchSong(songTitle);
@@ -29,7 +40,7 @@ public class SearchTest {
         // Search does not return empty
         assertNotNull(theSongs);
 
-        //searchSong returns song objects
+        // searchSong returns song objects
         for (Song song : theSongs) {
             assertTrue(song instanceof Song); // Ensure each element is of type Song
         }
@@ -43,10 +54,21 @@ public class SearchTest {
         // Search does not return empty
         assertNotNull(theAlbums);
 
-        //searchAlbum should return album objects
+        // searchAlbum should return album objects
         for (Album album : theAlbums) {
             assertTrue(album instanceof Album); // Ensure each element is of type Song
         }
+    }
+
+    @Test // Test search with an empty string
+    public void testingEmptyAlbumSearch() {
+        String albumTitle = "";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            api.searchAlbum(albumTitle);
+        });
+
+        assertEquals("Album title cannot be null or empty", exception.getMessage());
     }
 
     @Test
@@ -57,9 +79,20 @@ public class SearchTest {
         // Search does not return empty
         assertNotNull(theArtists);
 
-        //searchAlbum should return album objects
+        // searchAlbum should return album objects
         for (Artist artist : theArtists) {
             assertTrue(artist instanceof Artist); // Ensure each element is of type Song
         }
+    }
+
+    @Test
+    public void testingEmptyArtistSearch() {
+        String artistName = "";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            api.searchArtist(artistName);
+        });
+
+        assertEquals("artistName cannot be null or empty", exception.getMessage());
     }
 }
